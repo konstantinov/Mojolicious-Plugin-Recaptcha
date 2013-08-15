258d3b0ae630eb069e9005c6d200bf3f467228fc
Fayland Lam <fayland@gmail.com>
2013-08-15 21:12:14 +0800

    form => is required for content-type=application/x-www-form-urlencoded

--- lib/Mojolicious/Plugin/Recaptcha.pm
+++ lib/Mojolicious/Plugin/Recaptcha.pm
@@ -54,7 +54,7 @@ HTML
 			
 			my @post_data = (
 				'http://www.google.com/recaptcha/api/verify',
-				{ 
+				form => { 
 					privatekey => $conf->{'private_key'},
 					remoteip   => 
 						$self->req->headers->header('X-Real-IP')
